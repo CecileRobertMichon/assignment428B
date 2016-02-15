1ba91107fba667f43befcdbc4b0c10dcf256d77a
Cecile Robert-Michon <cecile.robert-michon@mail.mcgill.ca>
2016-02-15 11:58:56 -0500

    first commit

---
+++ Amp/app/src/test/java/com/example/cecilerobertm/amp/UnitTests.java
@@ -0,0 +1,23 @@
+package com.example.cecilerobertm.amp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by cecilerobertm on 16-02-15.
+ */
+public class UnitTests {
+    @Test
+    public void no_args() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+
+    public void too_many_args() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+
+    public void arg_types() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
