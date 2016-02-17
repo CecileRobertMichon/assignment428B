5eaf3f055ef41a2258ed38ddad611d987758f794
Cecile Robert-Michon <cecile.robert-michon@mail.mcgill.ca>
2016-02-17 10:27:54 -0500

    added function skeleton

--- Amp/app/src/main/java/com/example/cecilerobertm/amp/MainActivity.java
+++ Amp/app/src/main/java/com/example/cecilerobertm/amp/MainActivity.java
@@ -10,4 +10,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    private double lettermail(int length, int width, double weight, boolean standard) {
+        if (standard) {
+            if (length > 245 || length < 140) {
+                // TODO handle out of bounds standard length
+            } else if (weight < 3.0 || weight > 50.0) {
+                // TODO handle out of bounds standard weight
+            } else if (width < 90 || width > 156) {
+                // TODO handle out of bounds standard width
+            }
+        } else {
+            if (length > 380) {
+                // TODO handle out of bounds non-standard length
+            } else if (weight > 500) {
+                // TODO handle out of bounds non-standard weight
+            } else if (width > 270) {
+                // TODO handle out of bounds non-standard width
+            }
+        }
+        return 1;
+    }
 }
