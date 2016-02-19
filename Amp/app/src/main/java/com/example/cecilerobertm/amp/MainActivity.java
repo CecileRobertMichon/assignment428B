4778e02bac7a8b85f0a5cd99a7e4ba2ca35379c4
Cecile Robert-Michon <cecile.robert-michon@mail.mcgill.ca>
2016-02-19 11:07:53 -0500

    Fixed cost layout and function

--- Amp/app/src/main/java/com/example/cecilerobertm/amp/MainActivity.java
+++ Amp/app/src/main/java/com/example/cecilerobertm/amp/MainActivity.java
@@ -1,35 +1,78 @@
 package com.example.cecilerobertm.amp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.SpannableStringBuilder;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.cecilerobertm.amp.model.NonStandardEnvelope;
+import com.example.cecilerobertm.amp.model.StandardLettermail;
 
 public class MainActivity extends AppCompatActivity {
+
+    boolean nonstandard = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Switch s = (Switch) findViewById(R.id.switch_standard);
+        CompoundButton.OnCheckedChangeListener listener = new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b) {
+                    nonstandard = true;
+
+                } else {
+                    nonstandard = false;
+                }
+            }
+        };
+
+        if (s != null) {
+            s.setOnCheckedChangeListener(listener);
+        }
     }
 
+    public void buttonClick(View v) {
+        Intent intent;
+        switch (v.getId()) {
+            case R.id.calculate_button:
+                EditText length_input = (EditText) findViewById(R.id.length);
+                EditText width_input = (EditText) findViewById(R.id.width);
+                EditText weight_input = (EditText) findViewById(R.id.weight);
+                try {
+                    int length = Integer.valueOf(length_input.getText().toString());
+                    int width = Integer.valueOf(width_input.getText().toString());
+                    double weight = Integer.valueOf(weight_input.getText().toString());
+                    TextView r = (TextView) findViewById(R.id.result_text);
+                    if (!nonstandard) {
+                        try {
+                            StandardLettermail es = new StandardLettermail(length, width, weight);
+                            r.setText("$" + es.getCost());
+                        } catch (IllegalArgumentException e) {
+                            Toast.makeText(this, "Invalid Dimensions", Toast.LENGTH_SHORT).show();
+                        }
 
-    private double lettermail(int length, int width, double weight, boolean standard) {
-        if (standard) {
-            if (length > 245 || length < 140) {
-                // TODO handle out of bounds standard length
-            } else if (weight < 3.0 || weight > 50.0) {
-                // TODO handle out of bounds standard weight
-            } else if (width < 90 || width > 156) {
-                // TODO handle out of bounds standard width
-            }
-        } else {
-            if (length > 380) {
-                // TODO handle out of bounds non-standard length
-            } else if (weight > 500) {
-                // TODO handle out of bounds non-standard weight
-            } else if (width > 270) {
-                // TODO handle out of bounds non-standard width
-            }
+
+                    } else {
+                        try {
+                            NonStandardEnvelope ens = new NonStandardEnvelope(length, width, weight);
+                            r.setText("$" + ens.getCost());
+                        } catch (IllegalArgumentException e) {
+                            Toast.makeText(this, "Invalid Dimensions", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                } catch (NumberFormatException e) {
+                    Toast.makeText(this, "Invalid Dimensions", Toast.LENGTH_SHORT).show();
+                }
+                break;
         }
-        return 1;
     }
 }
